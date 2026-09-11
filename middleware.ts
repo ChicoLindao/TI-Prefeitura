e658f2c2b218df311864a14c4356e4fc7c1a6587
ChicoLindao <chicolima1996@gmail.com>
2026-09-10 22:14:11 -0300

    Segurança das rotas da api

--- middleware.ts
+++ middleware.ts
@@ -3,11 +3,16 @@ export { default } from "next-auth/middleware";
 // Aqui nós definimos quais rotas o "cão de guarda" vai proteger
 export const config = {
   matcher: [
-    /*
-     * Protege todas as rotas que começam com /dashboard
-     * O /:path* significa "e qualquer coisa que vier depois", 
-     * como /dashboard/internal, /dashboard/settings, etc.
-     */
-    "/dashboard/:path*"
+    /* PROTEÇÃO DAS TELAS VISUAIS */
+    "/dashboard/:path*",
+
+    /* PROTEÇÃO DAS PORTAS DOS FUNDOS (APIs PRIVADAS) */
+    "/api/external/:path*",
+    "/api/infra/:path*",
+    "/api/internal/:path*",
+    "/api/profile/:path*",
+    "/api/search/:path*",
+    "/api/settings/:path*",
+    "/api/users/:path*"
   ],
 };
